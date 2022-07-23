b4ce5a8e8f5cfdc345c328b6bd01eca79e9f5c92
olhazen <107193525+olhazen@users.noreply.github.com>
2022-07-23 19:59:04 +0100

    Added Axios

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function displayTemperature(response){
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+    console.log(response.data);
+}
+let apiKey = "63d044a506f804437a5b801028b410cc";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
