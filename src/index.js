f85e7edd4c8e42c682435052e21a420029a46c80
olhazen <107193525+olhazen@users.noreply.github.com>
2022-07-24 14:41:05 +0100

    Changed the city

--- src/index.js
+++ src/index.js
@@ -33,6 +33,6 @@ function formatDay(timestamp) {
   }
 
 let apiKey = "63d044a506f804437a5b801028b410cc";
-let city = "Lisbon";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
