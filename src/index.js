656c7020abaa0409618b8e902d460df488a576d6
olhazen <107193525+olhazen@users.noreply.github.com>
2022-07-27 22:16:24 +0100

    Updated HTML & Java

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
@@ -12,7 +36,7 @@ function formatDay(timestamp) {
     let forecastElement = document.querySelector("#forecast");
   
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (forecastDay, index) {
+    forecast.forEach(function (forecastDay, index) {
       if (index < 6) {
       forecastHTML =
         forecastHTML +
@@ -41,11 +65,9 @@ function formatDay(timestamp) {
   
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-    console.log(forecastHTML);
   }
 
   function getForecast(coordinates) {
-    console.log(coordinates);
     let apiKey = "63d044a506f804437a5b801028b410cc";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
@@ -88,36 +110,8 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
   }
-
-  function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
     
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  }
-    function displayCelsiusTemperature(event) {
-      event.preventDefault();
-      let temperatureElement = document.querySelector("#temperature");
-    
-      celsiusLink.classList.add("active");
-      fahrenheitLink.classList.remove("active");
-      let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-      temperatureElement.innerHTML = Math.round(celsiusTemperature);
-    }
-  
-  let celsiusTemperature = null;
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
 
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
   search("New York");
-  displayForecast();
